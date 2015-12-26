b1e43dd8e03404ab402e08e08d1ef3e66e960c5b
AVVS <v@aminev.me>
2015-12-26 17:23:04 +0300

    move around

--- variables.tf
+++ variables.tf
@@ -4,9 +4,18 @@ variable "platform" {
 }
 
 variable "user" {
+  description = "Platform -> user mapping"
   default = {
     ubuntu = "ubuntu"
   }
+}
+
+variable "ssh_pub_path" {
+  description = "ssh public key path"
+}
+
+variable "key_path" {
+  description = "ssh private key path to use with the google instance"
 }
 
 variable "region" {
@@ -29,14 +38,6 @@ variable "ami" {
 variable "networkName" {
   description = "network to use for the instances"
   default = "default"
-}
-
-variable "ssh_pub_path" {
-  description = "ssh public key path"
-}
-
-variable "key_path" {
-  description = "ssh private key path to use with the google instance"
 }
 
 variable "instance_type" {
